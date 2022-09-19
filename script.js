e3a9ceed3f5f5cfb17008efee0b42a06cd6feb6f
Muhammad Rehan <86519479+Rohtanza@users.noreply.github.com>
2022-09-19 05:34:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//navbar drop down and up
+
+const container = document.querySelector
+(`.container`);
+
+document.querySelector(`.open-navbar-icon`).
+addEventListener(`click`, ()=>{
+    container.classList.add(`change`);
+});
+
+document.querySelector(`.close-navbar-icon`).
+addEventListener(`click`, ()=>{
+    container.classList.remove(`change`);
+});
+
+//navbar color
+
+const colors = ['#6495ed', '#7fffd4',
+ '#ffa07a', '#f08080', '#afeeee'];
+
+let i = 0;
+
+Array.from(document.querySelectorAll
+    (".nav-link"))
+.forEach(item => {
+    item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
+
+
+//card price & back
+
+Array.from(document.querySelectorAll
+    (".navigation-button")).forEach
+    ((item) => {
+        item.onclick = () => {
+            item.parentElement.parentElement.
+                classList.toggle("change");
+        };
+    });
